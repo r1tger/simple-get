41204d1d61baf5df8ce7fb3bc2460284e061e060
Ritger Teunissen <github@ritger.nl>
2023-04-15 13:53:30 +0200

    Update version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='simpleget',
-    version='0.2.2',
+    version='0.3.0',
     description='Go get it!',
     url='https://github.com/r1tger/simple-get',
     author='Ritger Teunissen',
